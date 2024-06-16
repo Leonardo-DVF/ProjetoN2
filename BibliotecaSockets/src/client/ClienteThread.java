eaff97dacdc38d1dce276c394cdb18cdfd7dfd3e
Yann0410 <99684689+Yann0410@users.noreply.github.com>
2024-06-16 19:19:57 -0300

    Finalizando ClienteThread

--- BibliotecaSockets/src/client/ClienteThread.java
+++ BibliotecaSockets/src/client/ClienteThread.java
@@ -45,6 +45,63 @@ public class ClienteThread extends Thread{
                 }else if(!messageInputClient.contains("__EOF")) {
                     if(!comandos.contains(messageInputClient)) System.out.println(messageInputClient);
                 }
+                
+                // Se a mensagem for "CADASTRARLIVRO", solicita os dados do novo livro e envia para o servidor                
+                if(messageInputClient.equals("CADASTRARLIVRO")){
+                    PrintStream saida = new PrintStream(socketClient.getOutputStream());
+
+                    messageInputClient = readerServer.readLine();
+                    String idNovoCadastro = messageInputClient;
+                    
+                    String jsonDadosLivros = "";
+                    System.out.println("\nDigite os dados do livro\n");
+                    jsonDadosLivros = jsonDadosLivros + "{";
+                    jsonDadosLivros = jsonDadosLivros + "\"id\":" + idNovoCadastro + ",";
+                    jsonDadosLivros = jsonDadosLivros + "\"alugados\":\"0\",";
+                    System.out.print("Título:");
+                    jsonDadosLivros = jsonDadosLivros + "\"titulo\":\"" + scanner.nextLine() + "\",";
+                    System.out.print("Autor:");
+                    jsonDadosLivros = jsonDadosLivros + "\"autor\":\"" + scanner.nextLine() + "\",";
+                    System.out.print("Gênero:");
+                    jsonDadosLivros = jsonDadosLivros + "\"genero\":\"" + scanner.nextLine() + "\",";
+                    System.out.print("Quantidade de Exemplares:");
+                    jsonDadosLivros = jsonDadosLivros + "\"exemplares\":" + scanner.nextLine();
+                    jsonDadosLivros = jsonDadosLivros + "}";
+                    
+                    saida.println("DADOSLIVRO");
+                    saida.println(jsonDadosLivros);
+                }
+                
+                // Se a mensagem for "IDALUGAR", solicita o ID do livro a ser alugado e envia para o servidor                
+                if(messageInputClient.equals("IDALUGAR")){
+                    PrintStream saida = new PrintStream(socketClient.getOutputStream());
+                    saida.println("DADOSIDALUGAR");
+                    System.out.print("\nQual livro deseja alugar  (Digite o ID):");
+                    saida.println(scanner.nextLine());
+                }
+                
+                // Se a mensagem for "IDDEVOLVER", solicita o ID do livro a ser devolvido e envia para o servidor        
+                if(messageInputClient.equals("IDDEVOLVER")){
+                    System.out.print("\nQual livro deseja devolver (Digite o ID):");
+                    PrintStream saida = new PrintStream(socketClient.getOutputStream());
+                    saida.println("DADOSIDDEVOLVER");
+                    saida.println(scanner.nextLine());
+                }
+                
+                // Exibe mensagens de sucesso ou erro, dependendo da resposta do servidor
+                if(messageInputClient.equals("CADASTROSUCESSO")) System.out.println("\nLivro cadastrado!");                
+                if(messageInputClient.equals("ALUGADOSUCESSO")) System.out.println("\nLivro alugado!");
+                if(messageInputClient.equals("ALUGUELSEMSUCESSO")) System.out.println("\nLivro não foi alugado. Sem exemplar disponível. Tente outro livro!");
+                if(messageInputClient.equals("DEVOLVIDOSUCESSO")) System.out.println("\nLivro devolvido!");
+                if(messageInputClient.equals("DEVOLUCAOSEMSUCESSO")) System.out.println("\nLivro não foi devolvido. Verifique se o ID do livro foi digitado corretamente!");
+                if(messageInputClient.equals("DESCONHECIDO")) System.out.println("\nComando desconhecido. \nDigite \"AJUDA\" para informação dos comandos.\n\n");
+
+                // Se a mensagem contiver "__EOF", solicita um novo comando ao usuário e envia para o servidor                
+                if(messageInputClient.contains("__EOF")){
+                    PrintStream saida = new PrintStream(socketClient.getOutputStream());
+                    System.out.print("Digite o comando >>> ");
+                    saida.println(scanner.nextLine());
+                }
             }
         }catch(Exception e){
             System.out.println("Erro no client thread + " + e);
