df5a4550db6430c2fb15ae459d0a1ae4383cc393
Mateus Bittencourt <106128644+mateus-bittencourt-cc@users.noreply.github.com>
2024-06-16 23:36:38 -0300

    Update ServerSocketBiblioteca.java
    Adicionado método para geração de base de dados JSON para devolução de livros e construção de menu de ajuda
    
    - Implementado método para criar uma estrutura JSON com detalhes dos livros e atualizar a base de dados.
    - Adicionado método que gera um menu de ajuda formatado com comandos e descrições.

--- BibliotecaSockets/src/server/ServerSocketBiblioteca.java
+++ BibliotecaSockets/src/server/ServerSocketBiblioteca.java
@@ -199,3 +199,49 @@ public class ServerSocketBiblioteca {
                     }
                 }
             }//Final do for
+
+        JSONArray baseLivrosDevolucao = new JSONArray();
+            for (int i = 0; i < listaLivros.size(); i++) {
+                JSONObject jsonBaseLivroAlugados = new JSONObject();
+                jsonBaseLivroAlugados.put("id", listaLivros.get(i).getId());
+                jsonBaseLivroAlugados.put("titulo", listaLivros.get(i).getNomeLivro());
+                jsonBaseLivroAlugados.put("autor", listaLivros.get(i).getAutorLivro());
+                jsonBaseLivroAlugados.put("genero", listaLivros.get(i).getGeneroLivro());
+                jsonBaseLivroAlugados.put("exemplares", listaLivros.get(i).getNumeroExemplaresLivros());
+                jsonBaseLivroAlugados.put("alugados", listaLivros.get(i).getQntdAlugados());
+                baseLivrosDevolucao.put(jsonBaseLivroAlugados);
+            }
+
+            Boolean realizaDevolucao = new OperacoesLivros().atualizarBaseDadosLivro(baseLivrosDevolucao);
+
+            return realizaDevolucao;
+    }
+    
+    public static String menuAjuda(){
+        StringBuilder menuAjuda = new StringBuilder();
+        int i = 0;
+        
+        Formatter fmtMenuAjuda = new Formatter();
+        menuAjuda.append(fmtMenuAjuda.format("%150s","======================================================================================================================================================\n").toString());
+
+        fmtMenuAjuda = new Formatter();
+        menuAjuda.append(fmtMenuAjuda.format("%80s","MENU AJUDA - LISTA DE COMANDOS\n\n").toString());
+
+        fmtMenuAjuda = new Formatter();
+        fmtMenuAjuda.format("%5s %15s %110s\n\n", "NR", "COMANDO", "DESCRIÇÃO");
+        fmtMenuAjuda.format("%5s %15s %110s\n",++i,"AJUDA","Chama o menu de AJUDA com os comandos possíveis.");
+        fmtMenuAjuda.format("%5s %15s %110s\n",++i,"ALUGAR","Aluga um livro desejado. Vai pedir o ID do livro escolhido.");
+        fmtMenuAjuda.format("%5s %15s %110s\n",++i,"CADASTRAR","Cadastra um novo livro da base de dados. Pedirá dados do livro para cadastro.");
+        fmtMenuAjuda.format("%5s %15s %110s\n",++i,"DEVOLVER","Devolve um livro específico. Vai pedir o ID do livro alugado.");
+        fmtMenuAjuda.format("%5s %15s %110s\n",++i,"EXIT","Sai do sistema. Irá finalizar o cliente e o servidor! Cuidado.");
+        menuAjuda.append(fmtMenuAjuda.toString());
+
+        fmtMenuAjuda = new Formatter();
+        menuAjuda.append(fmtMenuAjuda.format("%150s","======================================================================================================================================================\n").toString());
+        menuAjuda.append("__EOF");
+
+        fmtMenuAjuda.close();
+        
+        return menuAjuda.toString();
+    }
+}
